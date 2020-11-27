a45384cb90b8d1a2abf5da44ac0047619257b9de
pablo <pablodelfantexp@gmail.com>
2020-11-26 22:11:18 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('header >*, section >*, footer >*', { 
+    delay: 10,
+    duration: 1000, 
+    reset:true,
+    
+    /* especifica 0-1 cuanto debe ppasar
+     del elemento para ser visible o no */
+    viewFactor: .1
+ });
+
+ // se puede deshabilitar en celular
